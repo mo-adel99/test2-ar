1f962e83f17ec3fcb0dd624c65db23b7a4feb7de
Mohamed Adel <134806641+mo-adel99@users.noreply.github.com>
2023-09-16 21:09:08 +0300

    a

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,9 +11,27 @@ document.addEventListener("DOMContentLoaded", function(){
   });
 });
   
-AFRAME.registerComponent("visibility-changer", {
-  init: function () {
-      this.el.setAttribute("visible", false)
-      this.el.sceneEl.addEventListener('zappar-visible', () => this.el.setAttribute("visible", true));
-  }
-});
+
+window.addEventListener("load", setup);
+
+function setup() {
+
+    const myImageGroup = document.getElementById("image-group");
+
+    let imageVisible = false;
+
+    myImageGroup.addEventListener("zappar-visible", () => {
+        // The image has appeared so show the group
+        myImageGroup.setAttribute("visible", "true");
+        imageVisible = true;
+    });
+
+    myImageGroup.addEventListener("zappar-notvisible", () => {
+        // The image has disappeared so hide the group after a short delay
+        imageVisible = false;
+        setTimeout(() => {
+            if (imageVisible === false) myImageGroup.setAttribute("visible", "false");
+        }, 500)
+    });
+
+}
